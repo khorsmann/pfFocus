ab11ad80fdfc9294da470658cf0f2fea56fd2e34
Karsten Horsmann <khorsmann@gmail.com>
2018-05-11 15:39:25 +0200

    alias can be none
    alias can be none

--- pfsense.py
+++ pfsense.py
@@ -79,7 +79,7 @@ class PfSenseAliasString(PfSenseString):
     @property
     def data(self):
         data = super().data
-        if hasattr_r(self.rootdoc.pfsense, 'aliases'):
+        if hasattr_r(self.rootdoc.pfsense, 'aliases.alias'):
             for alias in self.rootdoc.pfsense.aliases.alias:
                 if alias.name.string == data:
                     return {'alias': alias.data}
